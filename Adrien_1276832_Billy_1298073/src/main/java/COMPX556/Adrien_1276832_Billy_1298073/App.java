3492b5c4fce67efc7e0407266bdb158027cdf3be
Kamicandre <adrientan95@hotmail.com>
2020-10-14 10:26:31 +1300

    initial commit

---
+++ Adrien_1276832_Billy_1298073/src/main/java/COMPX556/Adrien_1276832_Billy_1298073/App.java
@@ -0,0 +1,13 @@
+package COMPX556.Adrien_1276832_Billy_1298073;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Adrien_1276832_Billy_1298073/src/test/java/COMPX556/Adrien_1276832_Billy_1298073/AppTest.java
@@ -0,0 +1,20 @@
+package COMPX556.Adrien_1276832_Billy_1298073;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

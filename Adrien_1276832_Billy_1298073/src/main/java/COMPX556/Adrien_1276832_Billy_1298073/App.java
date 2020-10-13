5652d296d64007d7793e1d5011fab5b03f0d0de4
Kamicandre <adrientan95@hotmail.com>
2020-10-14 12:00:32 +1300

    Update code to attempt to create a map of the excel sheet

--- Adrien_1276832_Billy_1298073/src/main/java/COMPX556/Adrien_1276832_Billy_1298073/App.java
+++ Adrien_1276832_Billy_1298073/src/main/java/COMPX556/Adrien_1276832_Billy_1298073/App.java
@@ -1,13 +1,65 @@
 package COMPX556.Adrien_1276832_Billy_1298073;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static ExcelReader ExcelRead;
+	public static void test() {
+		ExcelRead = new ExcelReader();
+		List<String> test1 = ExcelRead.getList();
+        System.out.print(test1);
+		for(int i=0;i<test1.size();i++) {
+			System.out.print(test1.get(i));
+		}
+	}
+	public static void boxRead() {
+		ExcelRead = new ExcelReader();
+		ArrayList<BOX> test2 = ExcelRead.getBox();
+        System.out.print(test2);
+		for(int i=0;i<test2.size();i++) {
+			System.out.print(test2.get(i));
+		}
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       System.out.println( "Hello World!" );
+       test();
+       boxRead();
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	public ExcelReader getExcelRead() {
+		return ExcelRead;
+	}
+	public void setExcelRead(ExcelReader excelRead) {
+		ExcelRead = excelRead;
+	}
 }
--- Adrien_1276832_Billy_1298073/src/main/java/COMPX556/Adrien_1276832_Billy_1298073/ExcelReader.java
+++ Adrien_1276832_Billy_1298073/src/main/java/COMPX556/Adrien_1276832_Billy_1298073/ExcelReader.java
@@ -2,7 +2,9 @@ package COMPX556.Adrien_1276832_Billy_1298073;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -14,13 +16,17 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelReader {
 	
 	private static final String NAME = "../../10x10_Test_Template.xlsx";
-
+	private static List<String> ant = new ArrayList<String>();
+	static BOX box;
+	static ArrayList<BOX> boxA = new ArrayList<BOX>();
 	public static void main(String[] args) {
 		try {
 			FileInputStream file = new FileInputStream(new File(NAME));
 			Workbook workbook = new XSSFWorkbook(file);
 			DataFormatter dataFormatter = new DataFormatter();
 			Iterator<Sheet> sheets = workbook.sheetIterator();
+			int r = 0;
+			int c = 0;
 			while(sheets.hasNext()) {
 				Sheet sh = sheets.next();
 				System.out.println("Sheet name is " + sh.getSheetName());
@@ -28,23 +34,66 @@ public class ExcelReader {
 				Iterator<Row> iterator = sh.iterator();
 				while(iterator.hasNext()) {
 					Row row = iterator.next();
+					r = r++;
 					Iterator<Cell> cellIterator = row.iterator();
 					while (cellIterator.hasNext()) {
 						Cell cell = cellIterator.next();
+						c = c++;
 						String cellValue = dataFormatter.formatCellValue(cell);
 //						if(cell.getCellType() == CellType.STRING) {
 //						}
+						box = new BOX(cellValue, r, c);
+						boxA.add(box);
+						ant.add(cellValue);
 						System.out.print(cellValue+"\t");
 					}
 					System.out.println();
 				}
 			}
 			workbook.close();
+			for(int i=0;i<ant.size();i++){
+				System.out.print(ant.get(i));
+			}
+			System.out.println(ant);
 		}
 		catch(Exception error){
 			error.printStackTrace();
 		}
-
 	}
+	
+	public List<String> getList() {
+		main(null);
+		return ant;
+	}
+	
+	public ArrayList<BOX> getBox() {
+		main(null);
+		return boxA;
+	}
+}
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+class BOX{
+	private String value;
+	private int row;
+	private int column;
+	
+	public BOX(String v, int r, int c) {
+		this.value = v;
+		this.row = r;
+		this.column = c;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	public int getColumn() {
+		return this.column;
+	}
 }
